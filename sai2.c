d1d05c0ad48e19b3d4e52dd153f49fb2da1e5659
saikiran-malyala <58409150+saikiran-malyala@users.noreply.github.com>
2019-12-06 12:32:48 +0530

    Add files via upload

---
+++ sai2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("enter the 2 nos");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("\n the sub is :%d",c);
+	return 0;
+}
+
---
+++ sai4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int a=100,b=30,c;
+c=a>b?a:b;
+printf("the biggestno is:%d",c);
+  return 0;
+}
