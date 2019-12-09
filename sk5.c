ac839a19504eb19dbc00bed3ceaf179a197c6d70
saikiran-malyala <58409150+saikiran-malyala@users.noreply.github.com>
2019-12-09 12:16:52 +0530

    Add files via upload

---
+++ sk5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int a=10,b=10;
+	printf("a<<2=%d",a<<2);
+	printf("b>>2=%d",b>>2);
+	return 0;
+	}
---
+++ sk6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+//int a=10,20,30;
+int b=(10,20,30);
+//printf("a<<2=%d",a);
+printf("\nb2=%d",b);
+return 0;
+}
